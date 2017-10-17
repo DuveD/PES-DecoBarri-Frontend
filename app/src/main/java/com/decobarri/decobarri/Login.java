347f6d2b12cbbcbb11c67c1f293fe40b30fa0051
inessgil <ines_sgil@hotmail.com>
2017-10-17 09:58:29 +0200

    Login activity created

--- app/src/main/java/com/decobarri/decobarri/Login.java
+++ app/src/main/java/com/decobarri/decobarri/Login.java
@@ -2,12 +2,22 @@ package com.decobarri.decobarri;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Login");
         setContentView(R.layout.activity_login);
+    }
+
+    //Comprova que l'usuari existeix
+    //Si existeix obre l'activity principal
+    //Si no mostra error
+    public void iniciar_sessio(View v) {
+
+        v.setVisibility(View.VISIBLE);
     }
 }
