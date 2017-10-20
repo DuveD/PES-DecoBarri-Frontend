4d59dc6682119a4ff242893c90ddf7094fcf4423
inessgil <ines_sgil@hotmail.com>
2017-10-20 09:51:58 +0200

    Login and register (DON'T WORK)

--- app/src/main/java/com/decobarri/decobarri/Login.java
+++ app/src/main/java/com/decobarri/decobarri/Login.java
@@ -8,10 +8,33 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.Block;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.MongoException;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+
+import java.lang.Object;
+import java.util.Objects;
+
+import com.mongodb.client.model.Filters;
+
+import org.bson.Document;
+
+
 public class Login extends AppCompatActivity {
 
     TextView error;
     EditText username, password;
+    MongoClientURI uri;
+    MongoClient mongoClient;
+    MongoDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +44,37 @@ public class Login extends AppCompatActivity {
         error = (TextView) findViewById(R.id.textView3);
         username = (EditText) findViewById(R.id.editText);
         password = (EditText) findViewById(R.id.editText2);
+
+        uri = new MongoClientURI( "mongodb://projectePES:PES12L@ds121565.mlab.com:21565/decorbarris" );
+        mongoClient = new MongoClient(uri);
+        db = mongoClient.getDatabase(uri.getDatabase());
     }
 
-    //Comprova que l'usuari existeix
-    //Si existeix obre l'activity principal
-    //Si no mostra error
     public void iniciar_sessio(View v) {
 
-        error.setVisibility(View.VISIBLE);
-        username.getText().clear();
-        password.getText().clear();
+        String user = username.getText().toString();
+        String pass = password.getText().toString();
+
+
+
+        try {
+            MongoCollection<Document> coll = db.getCollection("users");
+
+            BasicDBObject whereq = new BasicDBObject();
+            whereq.put("username", user);
+            MongoCursor<Document> c = coll.find(whereq).iterator();
+            if( c.hasNext() && Objects.equals(c.next().get("password").toString(), pass)){
+                TextView exito = (TextView) findViewById(R.id.textView5);
+                exito.setVisibility(View.VISIBLE);
+            }
+            else{
+                error.setVisibility(View.VISIBLE);
+                username.getText().clear();
+                password.getText().clear();
+            }
+        }catch (MongoException e) {
+            e.printStackTrace();
+        }
     }
 
     public void registre (View v) {
