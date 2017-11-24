1290c7f612e9027ed0fe99c77dffc672e1d165ba
Arnau Orriols <aog182@gmail.com>
2017-11-24 09:59:32 +0100

    ProjectActivity progress

--- app/src/main/java/com/decobarri/decobarri/main_menu/ProjectActivity.java
+++ app/src/main/java/com/decobarri/decobarri/main_menu/ProjectActivity.java
@@ -1,10 +1,8 @@
 package com.decobarri.decobarri.main_menu;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
-
-import com.decobarri.decobarri.BaseActivity;
 import com.decobarri.decobarri.R;
 import com.decobarri.decobarri.db_resources.Project;
 import com.decobarri.decobarri.db_resources.ProjectClient;
@@ -20,64 +18,53 @@ import retrofit2.converter.gson.GsonConverterFactory;
  * Created by arnauorriols on 10/11/17.
  */
 
-public class ProjectActivity extends BaseActivity {
+public class ProjectActivity extends AppCompatActivity {
 
     private TextView projNameText;
     private TextView descriptionText;
+    ProjectClient client;
+    Project project;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_project_info);
 
+        System.out.println("prova");
+
         projNameText = (TextView) findViewById(R.id.textProjName);
         descriptionText = (TextView) findViewById(R.id.descriptionText);
 
-        myTask task = new myTask();
-        task.execute(4);
+        Retrofit.Builder builder = new Retrofit.Builder()
+                .baseUrl("http://project-pes.herokuapp.com")
+                .addConverterFactory(GsonConverterFactory.create(new GsonBuilder().setLenient().create()));
+        Retrofit retrofit = builder.build();
+        client = retrofit.create(ProjectClient.class);
+
+        loadProjectInfo();
     }
 
-    private class myTask extends AsyncTask<Integer, Integer, String> {
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
+    private void loadProjectInfo() {
+        Call<Project> call = client.FindProjectById("5a0f1bf1b42109137235a5e3");
 
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-        }
-
-        @Override
-        protected String doInBackground(Integer... integers) {
-            Retrofit.Builder builder = new Retrofit.Builder()
-                    .baseUrl("http://project-pes.herokuapp.com")
-                    .addConverterFactory(GsonConverterFactory.create(new GsonBuilder().setLenient().create()));
-            Retrofit retrofit = builder.build();
-            ProjectClient client = retrofit.create(ProjectClient.class);
-            Call<Project> call = client.FindProjectById("5a0f1bf1b42109137235a5e3");
-
-            call.enqueue(new Callback<Project>() {
-                @Override
-                public void onResponse(Call<Project> call, Response<Project> response) {
-                    if (response.isSuccessful()) {
-                        Project p = response.body();
-                        System.out.println("Success : " + p.getName());
-                        projNameText.setText(p.getName());
-                        descriptionText.setText(p.getDescription());
-                    } else {
-                        System.out.println("Error: " + response.body());
-                    }
+        call.enqueue(new Callback<Project>() {
+            @Override
+            public void onResponse(Call<Project> call, Response<Project> response) {
+                if (response.isSuccessful()) {
+                    project = response.body();
+                    System.out.println("Success!! : " + project);
+                    projNameText.setText(project.getName());
+                    descriptionText.setText(project.getDescription());
+                } else {
+                    System.out.println("Error: " + response.body());
                 }
+            }
 
-                @Override
-                public void onFailure(Call<Project> call, Throwable t) {
-                    System.out.println("Error call : " + call.request().toString());
-                    System.out.println("Error throwable: " + t.getMessage());
-                }
-            });
-
-            return null;
-        }
+            @Override
+            public void onFailure(Call<Project> call, Throwable t) {
+                System.out.println("Error call : " + call.request().toString());
+                System.out.println("Error throwable: " + t.getMessage());
+            }
+        });
     }
 }
